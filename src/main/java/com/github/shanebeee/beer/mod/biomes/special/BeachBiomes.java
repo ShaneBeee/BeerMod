ec1dd9d5a9b313567750a2e86e8430d7c83507ad
ShaneBeee <shanebolenback@me.com>
2026-04-01 07:55:22 -0700

    Use vanilla beach in cold beach areas

--- src/main/java/com/github/shanebeee/beer/mod/biomes/special/BeachBiomes.java
+++ src/main/java/com/github/shanebeee/beer/mod/biomes/special/BeachBiomes.java
@@ -3,13 +3,15 @@ package com.github.shanebeee.beer.mod.biomes.special;
 import com.github.shanebeee.beer.mod.registry.BeerBiomes;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
 import org.jetbrains.annotations.NotNull;
 
 public class BeachBiomes {
 
     public static @NotNull ResourceKey<Biome> getBiome(int temp, int humidity, int weirdness) {
         return switch (temp) {
-            case 0, 1 -> BeerBiomes.COAST_FROZEN_BEACH;
+            case 0 -> BeerBiomes.COAST_FROZEN_BEACH;
+            case 1 -> Biomes.BEACH;
             case 2 -> weirdness == 0 ? BeerBiomes.COAST_COAST : BeerBiomes.COAST_BEACHY_COAST;
             case 3 -> humidity <= 2 ? BeerBiomes.COAST_PALM_BEACH : BeerBiomes.COAST_LUSH_COAST;
             default -> humidity <= 2 ? BeerBiomes.COAST_DRY_COAST : BeerBiomes.COAST_LUSH_COAST;
