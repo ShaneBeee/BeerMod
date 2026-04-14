12dfac20d4055294b54607dd4705fdd4aaef8d45
ShaneBee <shanebolenback@me.com>
2026-04-13 20:04:35 -0700

    Fix cave biomes under mushroom fields

--- src/main/java/com/github/shanebeee/beer/mod/biomes/continental/IslandBiomes.java
+++ src/main/java/com/github/shanebeee/beer/mod/biomes/continental/IslandBiomes.java
@@ -1,5 +1,6 @@
 package com.github.shanebeee.beer.mod.biomes.continental;
 
+import com.github.shanebeee.beer.api.biome.Continentalness;
 import com.github.shanebeee.beer.api.biome.Erosion;
 import com.github.shanebeee.beer.api.biome.Humidity;
 import com.github.shanebeee.beer.api.biome.PeaksAndValleys;
@@ -8,11 +9,16 @@ import com.github.shanebeee.beer.api.biome.Weirdness;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
+import org.jspecify.annotations.NonNull;
 
 @SuppressWarnings("unused")
 public class IslandBiomes {
 
     public static ResourceKey<Biome> getBiome(Temperature temp, Humidity humidity, Weirdness weirdness, PeaksAndValleys pv, Erosion erosion) {
+        return Biomes.MUSHROOM_FIELDS;
+    }
+
+    public static @NonNull ResourceKey<Biome> getIslandCave(Continentalness continentalness, Temperature temp, Humidity humidity, Weirdness weirdness, PeaksAndValleys pv) {
         return Biomes.MUSHROOM_FIELDS;
     }
 
