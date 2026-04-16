242cf0413d0592a355e98357a9e1ecb19057e03e
ShaneBeee <shanebolenback@me.com>
2026-04-16 14:20:04 -0700

    Update fabric-api

--- src/main/java/com/github/shanebeee/beer/mod/registration/TagRegistration.java
+++ src/main/java/com/github/shanebeee/beer/mod/registration/TagRegistration.java
@@ -4,13 +4,18 @@ import com.github.shanebeee.beer.mod.registry.tags.BeerBlockTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider.BlockTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.data.tags.TagAppender;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.data.tags.BlockItemTagAppender;
+import net.minecraft.references.BlockItemId;
+import net.minecraft.references.BlockItemIds;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import org.jspecify.annotations.NonNull;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public class TagRegistration extends BlockTagsProvider {
@@ -38,19 +43,28 @@ public class TagRegistration extends BlockTagsProvider {
 
         // Allow chickens to spawn on stone bricks
         valueLookupBuilder(BlockTags.ANIMALS_SPAWNABLE_ON)
-            .add(Blocks.STONE_BRICKS);
+            .add(BlockItemIds.STONE_BRICKS);
     }
 
     private BeerBuilder valueLookupBuilder(TagKey<Block> tag) {
-        TagAppender<Block> builder = builder(tag);
+        BlockItemTagAppender<Block> builder = builder(tag);
         return new BeerBuilder(builder);
     }
 
-    private record BeerBuilder(TagAppender<Block> builder) {
+    @SuppressWarnings({"UnusedReturnValue", "SameParameterValue"})
+    private record BeerBuilder(BlockItemTagAppender<Block> builder) {
 
         private BeerBuilder add(Block... blocks) {
             for (Block block : blocks) {
-                this.builder.add(block.builtInRegistryHolder().key());
+                Optional<ResourceKey<Block>> resourceKey = BuiltInRegistries.BLOCK.getResourceKey(block);
+                this.builder.add(resourceKey.orElseThrow());
+            }
+            return this;
+        }
+
+        private BeerBuilder add(BlockItemId... ids) {
+            for (BlockItemId id : ids) {
+                this.builder.add(id);
             }
             return this;
         }
