3fed4cfbafd0ffbb2645f31105a806c9cbc9eec6
ShaneBeee <shanebolenback@me.com>
2026-05-12 15:56:29 -0700

    Modify cave carvers

--- src/main/java/com/github/shanebeee/beer/mod/registration/CarverRegistration.java
+++ src/main/java/com/github/shanebeee/beer/mod/registration/CarverRegistration.java
@@ -5,6 +5,7 @@ import com.github.shanebeee.beer.api.registration.CarverDefinition;
 import com.github.shanebeee.beer.mod.registry.Carvers;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.UniformFloat;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -16,34 +17,30 @@ public class CarverRegistration extends BaseRegistration<ConfiguredWorldCarver<?
     public CarverRegistration(BootstrapContext<ConfiguredWorldCarver<?>> context) {
         super(Registries.CONFIGURED_CARVER, context);
 
-        CarverDefinition cave = CarverDefinition.caveBuilder(Carvers.CAVE, context)
-            .probability(0.15f)
-            .floorLevel(UniformFloat.of(-1.0f, -0.4f))
-            .horizontalRadiusMultiplier(UniformFloat.of(0.7f, 1.4f))
-            .lavaLevel(VerticalAnchor.aboveBottom(8))
-            .replaceable(BlockTags.OVERWORLD_CARVER_REPLACEABLES)
-            .verticalRadiusMultiplier(UniformFloat.of(0.8f, 1.3f))
-            .y(UniformHeight.of(
-                VerticalAnchor.aboveBottom(8),
-                VerticalAnchor.absolute(180)
-            ))
-            .yScale(UniformFloat.of(0.1f, 0.9f))
-            .build();
+        CarverDefinition cave = createCave(Carvers.CAVE,
+            0.35f,
+            VerticalAnchor.aboveBottom(8),
+            VerticalAnchor.absolute(180));
         register(cave);
 
-        CarverDefinition cave_extra = CarverDefinition.caveBuilder(Carvers.CAVE_EXTRA, context)
-            .probability(0.07f)
-            .floorLevel(UniformFloat.of(-1.0f, -0.4f))
-            .horizontalRadiusMultiplier(UniformFloat.of(0.7f, 1.4f))
-            .lavaLevel(VerticalAnchor.aboveBottom(8))
-            .replaceable(BlockTags.OVERWORLD_CARVER_REPLACEABLES)
-            .verticalRadiusMultiplier(UniformFloat.of(0.8f, 1.3f))
-            .y(UniformHeight.of(
-                VerticalAnchor.aboveBottom(8),
-                VerticalAnchor.absolute(47)
-            ))
-            .yScale(UniformFloat.of(0.1f, 0.9f))
-            .build();
+        CarverDefinition cave_extra = createCave(Carvers.CAVE_EXTRA,
+            0.5f,
+            VerticalAnchor.aboveBottom(8),
+            VerticalAnchor.absolute(0));
         register(cave_extra);
     }
+
+    private CarverDefinition createCave(ResourceKey<ConfiguredWorldCarver<?>> key, float probability, VerticalAnchor minY, VerticalAnchor maxY) {
+        return CarverDefinition.caveBuilder(key, getContext())
+            .probability(probability)
+            .replaceable(BlockTags.OVERWORLD_CARVER_REPLACEABLES)
+            .floorLevel(UniformFloat.of(-1.0f, -0.4f))
+            .lavaLevel(VerticalAnchor.aboveBottom(8))
+            .horizontalRadiusMultiplier(UniformFloat.of(1.9f, 2.4f))
+            .verticalRadiusMultiplier(UniformFloat.of(1.2f, 1.7f))
+            .y(UniformHeight.of(minY, maxY))
+            .yScale(UniformFloat.of(0.1f, 0.4f))
+            .build();
+    }
+
 }
